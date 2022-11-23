946505e361b3a1428604cfdfd4159b769bb8ac8f
dcanomainar10 <dcanomainar@gmail.com>
2022-11-23 11:12:48 +0100

    changes

--- timer.js
+++ timer.js
@@ -2,7 +2,8 @@ var x = setInterval(function() {
   const date_future = new Date(2022, 11, 23, 16, 00, 0, 0);
   const date_now = new Date();
 
-  seconds = Math.floor((date_future - (date_now))/1000);
+  var diffTime = date_future - (date_now);
+  seconds = Math.floor((diffTime)/1000);
   minutes = Math.floor(seconds/60);
   hours = Math.floor(minutes/60);
   days = Math.floor(hours/24);
@@ -11,7 +12,8 @@ var x = setInterval(function() {
   minutes = minutes-(days*24*60)-(hours*60);
   seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
 
-  days = Math.floor(days/13);
+  days = Math.floor(days/14);
+  days = 0;
 
   document.getElementById("main").innerHTML = days + " dias " + hours + " horas " + minutes + " minutos " + seconds + " segundos";
  
